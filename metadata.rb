4d787410e6a28dbd2e2b6a310e32fae68279ca83
Thorsten Fischer <thorsten@froschi.org>
2012-06-21 00:52:13 +0200

    Added dependencies for lucid, precise.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,13 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
 %w/ubuntu/.each { |os| supports os }
+
+depends          "libbind9", ">= 0.0.1"
+depends          "libcap", ">= 0.0.1"
+depends          "libdns", ">= 0.0.1"
+depends          "libgssapi-krb5", ">= 0.0.1"
+depends          "libisc", ">= 0.0.1"
+depends          "libisccc", ">= 0.0.1"
+depends          "libisccfg", ">= 0.0.1"
+depends          "libssl", ">= 0.0.1"
+depends          "libxml2", ">= 0.0.1"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,18 @@
+include_recipe "libbind9"
+include_recipe "libdns"
+include_recipe "libisc"
+include_recipe "libisccc"
+include_recipe "libisccfg"
+
 packages = Array.new
 
 case node[:lsb][:codename]
 when "lucid"
+  include_recipe "libcap"
+  include_recipe "libgssapi-krb5"
+  include_recipe "libssl"
+  include_recipe "libxml2"
+
   packages |= %w/
     bind9utils
   /
